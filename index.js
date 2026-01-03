9c46690361d4ed14e61d7f5d413f2e4a308a9d65
siddiq55 <msiddiq5565@gmail.com>
2026-01-03 11:16:35 +0500

    serverless for vercel created.

--- index.js
+++ index.js
@@ -100,5 +100,4 @@ app.post('/upload', upload.single('image'), async (req, res) => {
 
 
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, ()=> console.log("Server is running on PORT:" + PORT));
+export default app;
